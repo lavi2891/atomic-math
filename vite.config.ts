6222c59a5b3c8d3a0ffe4a0528323d0ed199172e
Lavi Hen Zion <lavi2891@gmail.com>
2026-02-02 17:03:44 +0200

    chore(config): add path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@domain": path.resolve(__dirname, "src/domain"),
+      "@ui": path.resolve(__dirname, "src/ui"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@copy": path.resolve(__dirname, "src/copy"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
+});
