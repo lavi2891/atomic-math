6960491675838a8916abca058060193c2b06b1a5
Lavi Hen Zion <lavi2891@gmail.com>
2026-03-11 00:47:07 +0200

    refactor(questions): use generator definitions as source of truth

--- src/app/questionPools.ts
+++ src/app/questionPools.ts
@@ -1,12 +1,12 @@
 import type { QuestionDefinition } from "@domain/questions/definitions";
-import { SIGNED_NUMBERS_QUESTIONS } from "@domain/questions/bank/SIGNED_NUMBERS";
 import { SIGNED_NUMBERS_GENERATED_QUESTIONS } from "@domain/questions/bank/SIGNED_NUMBERS.generated";
+import { SIGNED_NUMBERS_SAMPLE_QUESTIONS } from "@domain/questions/samples/SIGNED_NUMBERS.samples";
 import type { TopicId } from "@domain/topics/types";
 
 export function selectQuestionPool(topicId: TopicId): QuestionDefinition[] {
   switch (topicId) {
     case "SIGNED_NUMBERS":
-      return [...SIGNED_NUMBERS_QUESTIONS, ...SIGNED_NUMBERS_GENERATED_QUESTIONS];
+      return [...SIGNED_NUMBERS_GENERATED_QUESTIONS, ...SIGNED_NUMBERS_SAMPLE_QUESTIONS];
     default:
       return [];
   }
